aea08d55ff0f4adc67840ce90bd48780271f85bf
mitch <mitch01nguyen@gmail.com>
2022-11-30 09:32:17 -0600

    finsihes spring boot tutorial

---
+++ src/test/java/com/example/SpringBootPract/SpringBootPractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
